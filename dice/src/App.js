c0a6f612a646fc7e85b616cbc5510bc4dc045bca
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-11 00:45:20 +0530

    Adding the RollDice Component

--- dice/src/App.js
+++ dice/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Die from './Die';
-
+import RollDice from './RollDice';
 function App() {
   return (
     <div className="App">
-      <Die face='five' />
-      <Die face='six' />
-      <Die face='three' />
-      <Die face='two' />
+      <RollDice />
     </div>
   );
 }
